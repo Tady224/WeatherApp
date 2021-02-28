4fa4b75f65d7a805cb71cef5e435bbac277f308d
Tady224 <78107107+Tady224@users.noreply.github.com>
2021-02-28 14:58:18 +0100

    add JS

--- src/script.js
+++ src/script.js
@@ -1,10 +1,15 @@
 
 function displayTemp(response) {
+let cityElement =document.querySelector ("city");
+let conditionsElement= document.querySelector("conditions");
 let temperatureElement = document.querySelector ("temperature");
 temperatureElement.innerHTML = response.data.main.temp;
+cityElement.innerHTML = response.data.name;
+conditionsElement=response.data.weather[0].discription;
+
 }
 let apiKey = "5625fc4e2f1e1a9677dec96db881138a";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=Hamburg&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemp);
 
