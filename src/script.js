4e044f3f682c52981e61c7a6a7771ce5fac09bee
Tady224 <78107107+Tady224@users.noreply.github.com>
2021-03-05 19:56:01 +0100

    add changes

--- src/script.js
+++ src/script.js
@@ -1,15 +1,23 @@
 
-function displayTemp(response) {
-let cityElement =document.querySelector ("city");
-let conditionsElement= document.querySelector("conditions");
-let temperatureElement = document.querySelector ("temperature");
-temperatureElement.innerHTML = response.data.main.temp;
+function showWeather(response) {
+
+let temperatureElement = document.querySelector("#temperature");
+temperatureElement.innerHTML = Math.round(celsiusTemperature);  
+let cityElement = document.querySelector("#city");
 cityElement.innerHTML = response.data.name;
-conditionsElement=response.data.weather[0].discription;
+let conditionsElement = document.querySelector("#conditions");
+conditionsElement.innerHTML = response.data.weather[0].description;
+let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML = response.data.main.humidity; 
+let windElement = document.querySelector("#wind");
+windElement.innerHTML = Math.round(response.data.wind.speed) 
+celsiusTemperature = response.data.main.temp;
+
+ 
 
 }
 let apiKey = "5625fc4e2f1e1a9677dec96db881138a";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Hamburg&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemp);
+axios.get(apiUrl).then(showWeather);
 
